18329a98e1798d69c4b63e688f4d952320196f59
wrujel <wilfredo.rujel@hotmail.com>
2024-03-23 20:19:11 +0000

    [bot] Update report

--- utils/projects.ts
+++ utils/projects.ts
@@ -1,17 +1,60 @@
 export const admin_dashboard_next = {
   title: "Admin Dashboard Next",
+  projectUrl: "https://admin-dashboard-next-roan.vercel.app",
   repoUrl: "https://github.com/wrujel/admin-dashboard-next",
-  projectUrl: "https://admin-dashboard-next-roan.vercel.app/",
 };
-
 export const movies_search = {
   title: "Movies Search",
+  projectUrl: "https://movies-search-five.vercel.app",
   repoUrl: "https://github.com/wrujel/movies-search",
-  projectUrl: "https://movies-search-five.vercel.app/",
 };
-
 export const portfolio_web = {
   title: "Portfolio Web",
+  projectUrl: "https://portfolio-web-wrujel.vercel.app",
   repoUrl: "https://github.com/wrujel/portfolio-web",
-  projectUrl: "https://portfolio-web-wrujel.vercel.app/",
+};
+export const airbnb_clone = {
+  title: "Airbnb Clone",
+  projectUrl: "https://rental-app-delta.vercel.app",
+  repoUrl: "https://github.com/wrujel/airbnb-clone",
+};
+export const clock_app = {
+  title: "Clock App",
+  projectUrl: "https://clock-app-wrujel.vercel.app/",
+  repoUrl: "https://github.com/wrujel/clock-app",
+};
+export const django_crud_react = {
+  title: "Django Crud React",
+  projectUrl: "https://django-crud-react.onrender.com",
+  repoUrl: "https://github.com/wrujel/django-crud-react",
+};
+export const github_history = {
+  title: "Github History",
+  projectUrl: "https://github-history.vercel.app/",
+  repoUrl: "https://github.com/wrujel/github-history",
+};
+export const netflix_clone = {
+  title: "Netflix Clone",
+  projectUrl: "https://movies-app-wrujel.vercel.app/auth",
+  repoUrl: "https://github.com/wrujel/netflix-clone",
+};
+export const rest_api_et = {
+  title: "Rest Api Et",
+  projectUrl: "https://rest-api-et.onrender.com",
+  repoUrl: "https://github.com/wrujel/rest-api-et",
+};
+export const slider_static = {
+  title: "Slider Static",
+  projectUrl: "https://ephemeral-zuccutto-49ec06.netlify.app/",
+  repoUrl: "https://github.com/wrujel/slider-static",
+};
+export const tesla_landing = {
+  title: "Tesla Landing",
+  projectUrl: "https://sage-daffodil-4904c3.netlify.app/",
+  repoUrl: "https://github.com/wrujel/tesla-landing",
+};
+export const tetris_javascript = {
+  title: "Tetris Javascript",
+  projectUrl: "https://tetris-javascript-pi.vercel.app",
+  repoUrl: "https://github.com/wrujel/tetris-javascript",
 };
