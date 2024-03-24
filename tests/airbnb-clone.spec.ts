f0f1a7bf1ca463bbe716535aa39b179c9cfbc96f
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2024-03-24 15:43:02 -0500

    add test airbnb-clone

--- tests/airbnb-clone.spec.ts
+++ tests/airbnb-clone.spec.ts
@@ -1,13 +1,132 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 import { airbnb_clone as project } from "../utils/projects";
-    
+import { generateCredentials } from "../utils/random";
+import dotenv from "dotenv";
+dotenv.config();
+
 const TITLE = project.title;
 const URL_PATH = project.projectUrl;
-    
+const PASSWORD_TEST = process.env.PASSWORD_TEST;
+const GMAIL_TEST = process.env.GMAIL_TEST;
+const GITHUB_TEST1 = process.env.GITHUB_TEST1;
+const EMAIL_TEST = process.env.EMAIL_TEST;
+const credentials = generateCredentials();
+
 test.beforeEach(async ({ page }) => {
   await page.goto(URL_PATH);
 });
-    
-test(`${TITLE} - Test project`, async ({ page }) => {
-  // add code here
+
+test(`${TITLE} - Test home without logging in`, async ({ page }) => {
+  await page.goto("https://rental-app-delta.vercel.app/");
+  await expect(page.getByRole("img", { name: "logo" })).toBeVisible();
+  await expect(
+    page
+      .locator("div")
+      .filter({ hasText: /^AnywhereAny WeekAdd Guests$/ })
+      .first()
+  ).toBeVisible();
+  await expect(page.getByText("Airbnb your home")).toBeVisible();
+  await expect(page.locator(".p-4")).toBeVisible();
+  await expect(
+    page.getByText(
+      "BeachWindmillsModernCountrysidePoolsIslandsLakeSkiingCastlesCavesCampingArcticDe"
+    )
+  ).toBeVisible();
+  const count = await page.locator("//img[contains(@alt,'Listing')]").count();
+  expect(count).toBeGreaterThan(10);
+});
+
+test(`${TITLE} - Test email register`, async ({ page }) => {
+  await expect(page.locator(".p-4")).toBeVisible();
+  await page.locator(".p-4").click();
+  await page.getByText("Sign up").click();
+  await expect(page.getByText("Welcome to Airbnb")).toBeVisible();
+  await expect(page.getByText("Create your account")).toBeVisible();
+  await expect(page.locator("#email")).toBeVisible();
+  await expect(page.locator("#name")).toBeVisible();
+  await expect(page.locator("#password")).toBeVisible();
+  await page.locator("#email").click();
+  await page.locator("#email").fill(credentials.email);
+  await page.locator("#name").click();
+  await page.locator("#name").fill(credentials.username);
+  await page.locator("#password").click();
+  await page.locator("#password").fill(PASSWORD_TEST);
+  await expect(
+    page.getByRole("button", { name: "Continue", exact: true })
+  ).toBeVisible();
+  await Promise.all([
+    page.waitForResponse(
+      (res) => res.url().includes("register") && res.status() === 200
+    ),
+    page.getByRole("button", { name: "Continue", exact: true }).click(),
+  ]);
+  await expect(page.getByText("Login").first()).toBeVisible();
+});
+
+test(`${TITLE} - Test gmail login`, async ({ page }) => {
+  await page.locator(".p-4").click();
+  await page.getByText("Login").click();
+  await expect(
+    page.getByRole("button", { name: "Continue with Google" })
+  ).toBeVisible();
+  await expect(page.getByText("Login").first()).toBeVisible();
+  await page.getByRole("button", { name: "Continue with Google" }).click();
+  await page.getByLabel("Email or phone").click();
+  await page.getByLabel("Email or phone").fill(GMAIL_TEST);
+  await page.getByRole("button", { name: "Next" }).click();
+  await page.getByLabel("Enter your password").click();
+  await page.getByLabel("Enter your password").fill(PASSWORD_TEST);
+  await page.getByRole("button", { name: "Next" }).click();
+  await expect(page.locator(".p-4")).toBeVisible();
+  await page.locator(".p-4").click();
+  await expect(page.getByText("My trips")).toBeVisible();
+  await page.getByText("Logout").click();
+});
+
+test(`${TITLE} - Test github login`, async ({ page }) => {
+  await page.locator(".p-4").click();
+  await page.getByText("Login").click();
+  await expect(
+    page.getByRole("button", { name: "Continue with Github" })
+  ).toBeVisible();
+  await page.getByRole("button", { name: "Continue with Github" }).click();
+  await page.getByLabel("Username or email address").click();
+  await page.getByLabel("Username or email address").fill(GITHUB_TEST1);
+  await page.getByLabel("Username or email address").click();
+  await page.getByLabel("Password").click();
+  await page.getByLabel("Password").fill(PASSWORD_TEST);
+  await page.getByRole("button", { name: "Sign in", exact: true }).click();
+  await expect(page.locator(".p-4")).toBeVisible();
+  await page.locator(".p-4").click();
+  await expect(page.getByText("My trips")).toBeVisible();
+  await page.getByText("Logout").click();
+});
+
+test(`${TITLE} - Test home logged in`, async ({ page }) => {
+  await page.locator(".p-4").click();
+  await page.getByText("Login").click();
+  await page.locator("#email").click();
+  await page.locator("#email").fill(EMAIL_TEST);
+  await page.locator("#email").click();
+  await page.locator("#password").click();
+  await page.locator("#password").fill(PASSWORD_TEST);
+  await page.getByRole("button", { name: "Continue", exact: true }).click();
+  await expect(page.getByText("My trips")).toBeVisible();
+  await expect(page.getByText("My favorites")).toBeVisible();
+  await expect(page.getByText("My reservations")).toBeVisible();
+  await expect(page.getByText("My properties")).toBeVisible();
+  await expect(page.getByText("Airbnb my home")).toBeVisible();
+  await page
+    .locator("div")
+    .filter({ hasText: /^AnywhereAny WeekAdd Guests$/ })
+    .first()
+    .click();
+  await expect(page.getByText("Filters")).toBeVisible();
+  await expect(page.getByText("Where do you wanna go?")).toBeVisible();
+  await expect(
+    page
+      .locator("div")
+      .filter({ hasText: /^\+− Leaflet$/ })
+      .first()
+  ).toBeVisible();
 });
