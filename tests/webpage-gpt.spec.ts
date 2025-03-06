11e0672bfbddeab9ab1789b3e92c6e3b6a646e9c
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2025-03-05 21:31:24 -0500

    add webpage-gpt test

--- tests/webpage-gpt.spec.ts
+++ tests/webpage-gpt.spec.ts
@@ -1,13 +1,31 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 import { webpage_gpt as project } from "../utils/projects";
-    
+
 const TITLE = project.title;
 const URL_PATH = project.projectUrl;
-    
+
 test.beforeEach(async ({ page }) => {
   await page.goto(URL_PATH);
 });
-    
+
 test(`${TITLE} - Test project`, async ({ page }) => {
-  // add code here
+  await page.goto("https://webpage-gpt-wrujels-projects.vercel.app/");
+  await expect(page.getByText("Sign in")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Let's Build Something amazing" })
+  ).toBeVisible();
+  await expect(page.getByText("GPT-4o is a powerful AI model")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "The Future is Now and You" })
+  ).toBeVisible();
+  await expect(page.getByText("With GPT-4, the limits of")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "A lot is happening, We are" })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Do you want to step in to the" })
+  ).toBeVisible();
+  await expect(page.getByText("OpenAI © 2015 –")).toBeVisible();
+  await expect(page.getByText("This site is protected by")).toBeVisible();
+  await expect(page.getByRole("img", { name: "ai" })).toBeVisible();
 });
